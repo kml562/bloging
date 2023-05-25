67cb2bea6122076aae4152da5b622e99ec93a5ce
Sanket Munishwar <sanketmunishwar7@gmail.com>
2023-05-25 13:02:45 +0530

    models are created

---
+++ src/models/blogModel.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+const objectId = mongoose.Schema.Types.ObjectId
+
+const {Schema, model} = mongoose
+
+const blogSchema = new Schema({
+    title:{
+        type:String,
+        require:true
+    },
+    body:{
+        type:String,
+        require:true
+    },
+    authorId:{
+        type:objectId,
+        ref:"authorModel",
+        require:true
+    },
+    tags: [
+        {
+            type:String
+        }
+    ],
+    category:{
+        type:String,
+        require:true
+    },
+    subcategory:mongoose.Schema.Types.Mixed,
+    deletedAt: Date,
+    isDeleted: {
+        type:Boolean,
+        default:false
+    },
+    publishedAt: Date,
+    isPublished: {
+        type:Boolean,
+        default:false
+    }
+}, { timestamps: true})
+
+const blogModel = model('blogModel', blogSchema)
+
+export default blogModel
