5f3f346e0e062c7d982a43ea4e5bb462435f7a4f
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-05-29 14:45:28 +0530

    all done

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -1,18 +1,28 @@
 import { checkFormat } from "../utils/validation/validatior.js";
-
+import BlogModel from "../models/blogModel.js";
+import { response } from "express";
+import { compareSync } from "bcrypt";
 
 export const authorization = async (req, res, next) => {
   try {
-    const userTokenId = req.decodedToken.userKey;
-    const userKey = checkFormat(req.params.userKey);
+    const userTokenId = req.decodedToken.id;
+    
+    const userKey = checkFormat(req.params.blogId);
+    let data = {
+      _id:userKey, isDeleted:false
+    }
+    let Data = await BlogModel.findOne(data);
+    if (!Data) {
+     return  res.status(404).json({status:false, message:"blog not exsist"});
+    }
     if (!userKey) {
       return res.status(400).send({ status: false, message: 'Not authorized' });
     }
-
-    if (userTokenId !== userKey) {
+    console.log(userTokenId, Data.authorId.toString());
+    if (Data.authorId.toString() !== userTokenId) {
       return res.status(403).send({ status: false, message: 'You are not authorized' });
     }
-
+    req.authorId = userTokenId;
     next();
   } catch (error) {
     return res.status(500).send({ status: false, message: error.message });
--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -1,49 +1,51 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 const objectId = mongoose.Schema.Types.ObjectId;
 const { Schema, model } = mongoose;
 
-const blogSchema = new Schema({
-    title:{
-        type:String,
-        require:true
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      require: true,
     },
-    body:{
-        type:String,
-        require:true
+    body: {
+      type: String,
+      require: true,
     },
-    authorId:{
-        type:objectId,
-        ref:"authorModel",
-        require: true,
-  isValid:true,
+    authorId: {
+      type: objectId,
+      ref: "authorModel",
+      require: true,
+      isValid: true,
     },
     tags: [
-        {
-            type:String
-        }
+      {
+        type: String,
+      },
     ],
-    category:{
-        type:String,
-        require:true
+    category: {
+      type: String,
+      require: true,
     },
-    subcategory:mongoose.Schema.Types.Mixed,
- 
+    subcategory: [
+        {
+          type: String,
+        },
+      ],
+
     isDeleted: {
-        type:Boolean,
-        default:false
+      type: Boolean,
+      default: false,
+    },
+    isPublished: {
+      type: Boolean,
+      default: false,
     },
     publishedAt: Date,
-    isPublished: {
-        type:Boolean,
-        default:false
-    }
-}, { timestamps: true})
+    deletedAt: Date,
+  },
+  { timestamps: true }
+);
 
-
-
-
-
-
-
-const blogModel = model('blogModel', blogSchema);
+const blogModel = model("blogModel", blogSchema);
 export default blogModel;
